364d746cc44d4758050a77aec856b2371ca14bbc
Justin_karso <justin.karsopawiro001@fclive.nl>
2021-01-04 22:39:12 +0100

    webpack change

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-
 module.exports = {
     entry: ['@babel/polyfill', path.join(__dirname, 'src', 'index.js')],
     output: {
